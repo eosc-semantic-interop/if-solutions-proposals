2fb68c3f531fa65a0e5185c05c831a64cd5b1f80
Marc Portier <marc.portier@gmail.com>
2026-07-16 15:53:15 +0200

    avoid error by trying to produce pdf for the master-link.md file

--- proposals/radical-transparency/linkset-usage-patterns/mkpdf.sh
+++ proposals/radical-transparency/linkset-usage-patterns/mkpdf.sh
@@ -7,7 +7,7 @@ cd "$SCRIPT_DIR"
 ./mkln.sh 
 
 # run over the markdown files in the folder and generate a PDF for each one
-mds=$(find . -name '*.md' | sort)
+mds=$(find . -name '??-*.md' | sort)
 for md in $mds; do
     pdf="${md%.md}.pdf"
     echo "Generating PDF for $md -> $pdf"
